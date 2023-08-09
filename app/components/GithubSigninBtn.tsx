d4b5bcab54ba27b0d653d349aceab076632dda80
mohammadAminSH1524 <harrywill1524@mailfa.com>
2023-08-09 13:44:32 +0430

    next-auth with github-provider and mongodb database added

--- app/components/GithubSigninBtn.tsx
+++ app/components/GithubSigninBtn.tsx
@@ -2,16 +2,13 @@
 import { signIn } from "next-auth/react";
 import { BsGithub } from "react-icons/bs";
 
-
 export default function GithubSigninBtn() {
   return (
     <div
       className="flex items-center gap-1 justify-center text-2xl px-6 py-1.5 rounded-lg
       bg-primaryText text-white"
     >
-      
-        <BsGithub />
-      
+      <BsGithub />
       <button
         type="button"
         className="rounded-lg p-2 bg-primaryText text-white "
